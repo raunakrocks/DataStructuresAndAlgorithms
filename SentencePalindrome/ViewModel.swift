9a80d7a953dc1dd225c7c93fe203a7aaf4f543cf
Raunak Talwar <rtalwar@expedia.com>
2018-09-03 22:55:10 +0530

    Initialize the project.

---
+++ SentencePalindrome/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  SentencePalindrome
+//
+//  Created by Raunak Talwar on 03/09/18.
+//  Copyright © 2018 Raunak Talwar. All rights reserved.
+//
+
+import Foundation
+struct ViewModel {
+    
+}
